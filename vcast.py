5a5ea8bb059defcd3f4628932eb9429fcc4b02c1
Antonio Lima <anto87@gmail.com>
2009-01-25 22:40:28 +0000

    Primo commit

---
+++ vcast.py
@@ -0,0 +1,22 @@
+from restful_lib import Connection
+import re
+
+class Account(object):
+    """A vcast account"""
+
+    def __init__(self, username, password):
+        url = 'http://www.vcast.it/faucetpvr/api/1.0/server_rest.php'
+        self.connection = Connection(url)
+        self.connection.add_rest_credentials(username, password)
+
+    def get_channels(self):
+        return self.connection.request_get('/channels')
+
+    def get_recordings(self):
+        return self.connection.request_get('/recordings')
+
+    def get_feed(self):
+        """Return the RSS feed of avaible recordings"""
+        feed = self.connection.request_get('/feed')['body']
+        feed = re.sub(r'\\(.)', r'\1', feed)[13:-3]
+        return feed
