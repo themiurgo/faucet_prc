9532e9a6e32f5f20650e583a0f533b358573c7c0
Daniele Marletta <danielemar86@gmail.com>
2009-02-17 09:44:56 +0000

    Inizia il popolamento di RegisterDialog

--- RecorderDialog.py
+++ RecorderDialog.py
@@ -1,5 +1,7 @@
 import wx
 
+source=['Radio','TV']
+
 # Questa classe rappresenta la finestra che crea una
 # nuova registrazione. Ancora in fase di progettazione
 
@@ -7,39 +9,44 @@ class RecorderDialog(wx.Dialog):
     def __init__(
             self, parent, ID, title, size=wx.DefaultSize, pos=wx.DefaultPosition, 
             style=wx.DEFAULT_DIALOG_STYLE):
-        wx.Dialog.__init__(self, parent, -1)
+        wx.Dialog.__init__(self, parent, -1,title="Creazione Nuova Registrazione")
         
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         
-        label1 = wx.StaticText(self, -1, "This is a wx.Dialog")
-        label1.SetHelpText("This is the help text for the label")
-        mainSizer.Add(label1, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+        header = wx.StaticText(self, -1, "Inserisci tutti i campi per creare\n la nuova registrazione",style=wx.ALIGN_CENTRE)
+        header.SetHelpText("Per modificare i canali visualizzatti vai su preferenze")
+        mainSizer.Add(header, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
 
-        box = wx.BoxSizer(wx.HORIZONTAL)
-
-        label2 = wx.StaticText(self, -1, "Field #1:")
-        label2.SetHelpText("This is the help text for the label")
-        box.Add(label2, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
-
-        text = wx.TextCtrl(self, -1, "", size=(80,-1))
-        text.SetHelpText("Here's some help text for field #1")
-        box.Add(text, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
-        self.text1 = text       
+    # 1st Level -------------------------
         
-        mainSizer.Add(box, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+        firstBox = wx.BoxSizer(wx.HORIZONTAL)
 
-        box = wx.BoxSizer(wx.HORIZONTAL)
+        title = wx.StaticText(self, -1, "Titolo")
+        title.SetHelpText("Nome del programma che vuoi registrare")
+        firstBox.Add(title, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
 
-        label3 = wx.StaticText(self, -1, "Field #2:")
-        label3.SetHelpText("This is the help text for the label")
-        box.Add(label3, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
-
-        text = wx.TextCtrl(self, -1, "", size=(80,-1))
-        text.SetHelpText("Here's some help text for field #2")
-        box.Add(text, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
-        self.text2=text
+        titleText = wx.TextCtrl(self, -1, "", size=(80,-1))
+        titleText.SetHelpText("Nome del programma che vuoi registrare")
+        firstBox.Add(titleText, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+        #self.text1 = text       
         
-        mainSizer.Add(box, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+        mainSizer.Add(firstBox, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+    
+    
+     # 2nd Level -------------------------
+        
+        secondBox = wx.BoxSizer(wx.HORIZONTAL)
+
+        typeLabel = wx.StaticText(self, -1, "Tipo di registrazione")
+        typeLabel.SetHelpText("Puo' essere 'Radio' o 'TV'")
+        secondBox.Add(typeLabel, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        typeCB = typeComboBox(self)
+        typeCB.SetHelpText("A seconda dell'opzione selezionata, si modificheranno i canali")
+        secondBox.Add(typeCB, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+        #self.text2=text
+        
+        mainSizer.Add(secondBox, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
         
         box = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -82,3 +89,19 @@ class RecorderDialog(wx.Dialog):
     def GetValues(self):
         result = (self.text1.GetValue() ,self.text2.GetValue() ,self.text3.GetValue())
         return result
+        
+class typeComboBox(wx.ComboBox):
+    def __init__(self,parent):
+        wx.ComboBox.__init__(self,parent,500,choices=source,style=wx.CB_READONLY)
+        
+        #source = ['Radio','TV']
+        self.SetValue(source[1])
+
+        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox)
+    # When the user selects something, we go here.
+    def EvtComboBox(self, evt):
+        #cb = evt.GetEventObject()
+        data = evt.GetString()
+        #print data
+
+        
